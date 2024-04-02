08f62d7a46477a0fc25eb0a52410dd62d9dc5343
rkohnovets <61547543+rkohnovets@users.noreply.github.com>
2024-04-02 21:18:20 +0500

    feat/add docker (#1)
    * add docker
    
    * docker compose не нужен, юзаем kubernetes

--- config.js
+++ config.js
@@ -11,11 +11,12 @@ const port = process.env.PORT || 5000
 //      1.2) using the link above generate keys (min length 2048)
 //      1.3) copy and paste the generated keys inside the files
 // 2) using script 'generate-keys' from package.json:
-//      2.0) install openssl (on windows it's not so easy, on linux idk)
+//      2.0) install openssl (on windows it's not so easy, but possible)
 //      2.1) delete directory 'keys' (if there is)
 //      2.2) run command 'npm run generate-keys' in terminal (no passphrase)
 const privateKey = fs.readFileSync(path.join(__dirname, 'keys', 'rsa.key'), 'utf8')
 const publicKey = fs.readFileSync(path.join(__dirname, 'keys', 'rsa.key.pub'), 'utf8')
+
 // to use https on localhost you can generate your own certificate:
 // 1) install openssl
 // 2) delete directory 'cert'
@@ -25,9 +26,8 @@ const publicKey = fs.readFileSync(path.join(__dirname, 'keys', 'rsa.key.pub'), '
 const key = fs.readFileSync('./cert/key.pem');
 const cert = fs.readFileSync('./cert/cert.pem');
 
-// вообще это нужно как-то из переменных окружения доставать
-// или еще как-то, а не прост вписывать, но пока что так
-// (да я, где-то на энглише комменты, где-то на русском)
+// MongoDB data should be stored not in code, 
+// maybe in environment variables or using some other way
 const mongo_user = 'user'
 const mongo_pass = '123pass789'
 const mongo_cluster_url = 'myfreecluster.ugjdqge.mongodb.net/'
